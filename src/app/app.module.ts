bacc70c50957c03ce0107566d9f68aee5aa3a4c2
adriaqf <adriaqf@gmail.com>
2022-06-16 10:39:15 +0200

    body angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './component/index/index/index.component';
+import { MenuComponent } from './component/menu/menu/menu.component';
+import { LoanComponent } from './component/loan/loan.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { BookdetailsComponent } from './component/bookdetails/bookdetails.component';
+import { HomeComponent } from './component/home/home.component';
+import { ValueComponent } from './component/value/value.component';
+import { ProfileuserComponent } from './component/profileuser/profileuser.component';
+import { ProfileadminComponent } from './component/profileadmin/profileadmin.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ExchangeComponent } from './component/exchange/exchange.component';
+import { BookcommentComponent } from './component/bookcomment/bookcomment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    MenuComponent,
+    LoanComponent,
+    SidebarComponent,
+    NavbarComponent,
+    SignupComponent,
+    SigninComponent,
+    BookdetailsComponent,
+    HomeComponent,
+    ValueComponent,
+    ProfileuserComponent,
+    ProfileadminComponent,
+    FooterComponent,
+    ExchangeComponent,
+    BookcommentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/profileuser/profileuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profileuser',
+  templateUrl: './profileuser.component.html',
+  styleUrls: ['./profileuser.component.css']
+})
+export class ProfileuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
