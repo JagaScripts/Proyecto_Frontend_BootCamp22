1a4ad7001d5decd9ff0756f6ce777bdae1bcd720
adriaqf <adriaqf@gmail.com>
2022-06-18 14:39:10 +0200

    html update

--- src/app/component/bookdetails/bookdetails.component.ts
+++ src/app/component/bookdetails/bookdetails.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-bookdetails',
   templateUrl: './bookdetails.component.html',
-  styleUrls: ['./bookdetails.component.css']
+  styleUrls: ['./bookdetails.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BookdetailsComponent implements OnInit {
 
