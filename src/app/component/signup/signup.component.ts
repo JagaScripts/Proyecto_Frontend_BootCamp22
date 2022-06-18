f2de68ca44ccf10e5e0b9557f3b88c67288182c0
adriaqf <adriaqf@gmail.com>
2022-06-18 15:25:08 +0200

    update html

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SignupComponent implements OnInit {
 
