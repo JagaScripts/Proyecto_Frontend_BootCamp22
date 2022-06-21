ebd65bf55346548de65cc857f93f7627d1585166
adriaqf <adriaqf@gmail.com>
2022-06-21 18:54:58 +0200

    Module dialog

--- src/app/component/tablebooks/tablebooks.component.ts
+++ src/app/component/tablebooks/tablebooks.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 @Component({
   selector: 'app-tablebooks',
   templateUrl: './tablebooks.component.html',
   styleUrls: ['./tablebooks.component.css']
 })
 export class TablebooksComponent implements OnInit {
-
-  constructor() { }
+  animal!: string;
+  name!: string;
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
   }
+  openDialog(): void { // pasar el id por el constructor
 
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '550px',
+      data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+      console.log(this.animal);
+
+    });
+  }
 }
+
+
