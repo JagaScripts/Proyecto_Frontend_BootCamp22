6de1f1e1c21397ca6c2d2c8c944ecd8302c469db
JagaScripts <jagascripts@gmail.com>
2022-06-23 09:59:15 +0200

    Interceptor correto

--- src/app/helpers/auth.interceptor.ts
+++ src/app/helpers/auth.interceptor.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const TOKEN_HEADER_KEY = `Authorization`;
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
 
@@ -15,14 +16,21 @@ export class AuthInterceptor implements HttpInterceptor{
   private addToken(request: HttpRequest<any>) {
     const token = window.sessionStorage.getItem("auth-token");
     console.log(token, ' token');
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          token,
-        },
-      });
+    if (token != null) {
+      request = request.clone({ headers: request.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+      console.log(request.headers.getAll(TOKEN_HEADER_KEY) + " petición");
+      console.log(request.url + " petición");
       return request;
     }
+    //forma alternativa de añadir el token falta testear
+    // if (token) {
+    //   request = request.clone({
+    //     setHeaders: {
+    //       TOKEN_HEADER_KEY: `Bearer ${token}`
+    //     },
+    //   });
+    //   return request;
+    // }
     return request;
   }
 }
