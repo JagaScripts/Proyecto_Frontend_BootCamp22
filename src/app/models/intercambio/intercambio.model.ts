d049f0081134e3b8a50f822574c2040844af2340
adriaqf <adriaqf@gmail.com>
2022-06-24 15:06:02 +0200

    interfaces creadas, menos notificacion/notificar

---
+++ src/app/models/intercambio/intercambio.model.ts
@@ -0,0 +1,6 @@
+export interface Intercambio {
+  id:number;
+  libro_1_id:number;
+  libro_2_id:number;
+  intercambiar_id:number;
+}
---
+++ src/app/models/valoracion/valoracion.model.ts
@@ -0,0 +1,6 @@
+export interface Valoracion {
+  id:number;
+  usuario_id:number;
+  libro_id:number;
+  valorar_id:number;
+}
