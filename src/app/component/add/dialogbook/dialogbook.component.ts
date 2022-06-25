63c8288005ea5430075d3fe473cfa925ffa8e0b8
adriaqf <adriaqf@gmail.com>
2022-06-25 21:51:40 +0200

    implement Editorial

--- src/app/component/add/dialogbook/dialogbook.component.ts
+++ src/app/component/add/dialogbook/dialogbook.component.ts
@@ -1,14 +1,13 @@
+import { Book } from 'src/app/models/book/book.model';
+import { Rol } from 'src/app/models/enum/rol/rol.model';
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
   MatDialog,
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
-import { Book } from 'src/app/models/book/book.model';
-
-import { Rol } from 'src/app/models/enum/rol/rol.model';
-import { DialogComponent } from '../../dialog/dialog.component';
-
+import { EditorialService } from 'src/app/services/editorial/editorial.service';
 
 @Component({
   selector: 'app-dialogbook',
@@ -20,7 +19,16 @@ export class DialogbookComponent implements OnInit {
     id: 0,
     cantidad_veces_reservado: 0,
     url_img: '',
-    usuario: {id:0, username: '', password: '', email: '' , role: Rol.ADMIN , edad: new Date(), url_imagen: '', activo: '1' },
+    usuario: {
+      id: 0,
+      username: '',
+      password: '',
+      email: '',
+      role: Rol.ADMIN,
+      edad: new Date(),
+      url_imagen: '',
+      activo: '1',
+    },
     autor: '',
     titulo: '',
     isbn: '',
@@ -28,17 +36,38 @@ export class DialogbookComponent implements OnInit {
     categoria: '',
     disponible: '',
     descripcion: '',
+    editorial: {id:0, nombre:''}
   };
 
+  pushedAutor = 0;
+  pushedTitulo = 0;
+  pushedIsbn = 0;
+  pushedEdad = 0;
+  pushedCategoria = 0;
+  pushedDisponible = 0;
+  pushedDescripcion = 0;
+  pushedEditorial = 0;
+
+  editoriales: any = [];
   constructor(
     public dialog: MatDialog,
     public dialogRef: MatDialogRef<DialogbookComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
-    //dialogRef.disableClose = true;
-  }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private editorialService: EditorialService
+  ) {}
   libros: any = [];
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.editorialService.list().subscribe({
+      next:(result: any) => {
+        this.editoriales = result;
+      },error:(resultError: Error) => {
+        console.log(
+          `Nombre del error: ${resultError.name}, Mensaje del error: ${resultError.message}, Pila del error: ${resultError.stack}`
+        );
+      }
+    });
+  }
+
 
   onNoClick(): void {
     //data: { data, key, position },
@@ -50,4 +79,82 @@ export class DialogbookComponent implements OnInit {
 
     this.dialogRef.close(this.newEntry);
   }
+
+
+  controlForm(
+    value: string,
+    controlEmpty: boolean,
+    controlEmail?: boolean,
+    controlIsNumber?: boolean
+  ) {
+    let result = true;
+    if (controlEmpty) {
+      //no vacio
+      let test1 = value.replace(/\s{2,}/g, ' ').trim();
+      if (test1 === '') {
+        result = false;
+      }
+    }
+
+    if (controlEmail) {
+      //correo electronico
+      let regexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      if (regexp.test(value)) {
+        result = false;
+      }
+    }
+
+    if (controlIsNumber) {
+      //que sea numero
+      if (this.isNum(value)) {
+        result = false;
+      }
+    }
+    console.log(result);
+
+    return result;
+  }
+  isNum(val: any) {
+    return isNaN(val);
+  }
+
+  pushed1Time(key: string) {
+    switch (key) {
+      case 'autor':
+        this.pushedAutor++;
+        break;
+      case 'titulo':
+        this.pushedTitulo++;
+        break;
+      case 'isbn':
+        this.pushedIsbn++;
+        break;
+      case 'edad':
+        this.pushedEdad++;
+        break;
+      case 'categoria':
+        this.pushedCategoria++;
+        break;
+      case 'disponible':
+        this.pushedDisponible++;
+        break;
+      case 'editorial':
+        this.pushedEditorial++;
+        break;
+        case 'descripcion':
+          this.pushedDescripcion++;
+        break;
+      default:
+        break;
+
+
+    }
+      console.log(this.pushedAutor);
+  }
+
+  addEditorial(){
+    this.editorialService.add
+
+  }
+
 }
