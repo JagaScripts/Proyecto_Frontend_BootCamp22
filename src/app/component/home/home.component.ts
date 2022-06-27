dc2b26fe8c4068ba9b0b1f76b5e82b67a9bf9688
JagaScripts <jagascripts@gmail.com>
2022-06-27 18:27:48 +0200

    Home con las valoraciones ya funcionan

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Token } from 'src/app/models/token/token.model';
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/models/book/book.model';
-import { Rol } from 'src/app/models/enum/rol/rol.model';
 import { User } from 'src/app/models/user/user.model';
 import { LoginService } from 'src/app/services/auth/login.service';
 import { BookService } from 'src/app/services/book/book.service';
 import { ValoracionService } from 'src/app/services/valoracion/valoracion.service';
 import { Valoracion } from 'src/app/models/valoracion/valoracion.model';
 import { Estrellas } from 'src/app/models/enum/estrellas/estrellas.model';
-import { FormControl, FormGroup } from '@angular/forms';
 
+export interface LibroValoracion{
+  libro: Book;
+  valoracion: number;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -20,15 +22,18 @@ export class HomeComponent implements OnInit {
   // libro: Book;
   isLoggedIn: boolean = false;
   guess: User;
-  libros!: Book[];
-
-  starForm = new FormGroup({
-    rating: new FormControl()
-  });
+  // libros!: Book[];
+  librosValoraciones: LibroValoracion[] = [];
 
   constructor(private bookService: BookService,
               private loginService: LoginService,
               private valoracionService: ValoracionService) {
+    // this.libroValoracion = {
+    //   autor: "",
+    //   titulo: "",
+    //   url_img: "",
+    //   valoracion: 0
+    // }
     // this.libro = {
     //   id: 0,
     //   autor: "",
@@ -80,8 +85,7 @@ export class HomeComponent implements OnInit {
         .subscribe(
           {
             next: (libros: Book[]) => {
-              this.libros = libros;
-              this.setValorationBooks(this.libros);
+              this.setValorationBooks(libros);
             },
             error: (resultError: Error) => {
               console.log(`Nombre del error: ${resultError.name}, Mensaje del error: ${resultError.message}, Pila del error: ${resultError.stack}`);
@@ -92,14 +96,13 @@ export class HomeComponent implements OnInit {
 
 
 
-  listValorationByLibro(libro: Book, numreroLibro: number): void{
+  listValorationByLibro(libro: Book, numeroLibro: number): void{
     this.valoracionService.listByBook(libro)
         .subscribe(
           {
             next: (valoraciones: Valoracion[]) => {
-              console.log(valoraciones);
-
-              this.setAverageValoration(valoraciones, numreroLibro);
+              // console.log(valoraciones);
+              this.setAverageValoration(libro, valoraciones);
             },
             error: (resultError: Error) => {
               console.log(`Nombre del error: ${resultError.name}, Mensaje del error: ${resultError.message}, Pila del error: ${resultError.stack}`);
@@ -108,12 +111,16 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  setAverageValoration(valoraciones: Valoracion[], numreroLibro: number): void{
+  setAverageValoration(libro: Book, valoraciones: Valoracion[]): void{
     let avgStars = 0;
     // console.log(Array.isArray(valoraciones) && !valoraciones.length);
     // console.log(!valoraciones.length);
     // console.log(Array.isArray(valoraciones));
-
+    // console.log(numreroLibro + " numreroLibro");
+    // const objToRate: {[k : string]: any} = {};
+    // objToRate[`rating${numreroLibro}`] = avgStars;
+    //this.starForm.setValue(objToRate);
+    // console.log(Object.entries(objToRate));
     if (!(Array.isArray(valoraciones) && !valoraciones.length)) {
 
         valoraciones.forEach(function (valoracion) {
@@ -127,12 +134,27 @@ export class HomeComponent implements OnInit {
       // console.log(Math.round(avgStars /= valoraciones.length));
 
       avgStars = Math.round(avgStars /= valoraciones.length);
-      console.log(`${avgStars}`);
-      console.log("valor radio " + this.starForm.get(`rating`)?.value);
+      // console.log(`${avgStars}`);
+      // console.log("valor radio " + this.starForm.get(`rating`)?.value);
 
-      this.starForm.setValue({ "rating": avgStars.toString() });
+
+
+      // objToRate[`rating${numreroLibro}`] = avgStars.toString();
+
+      // this.starForm.setValue({ rating1: avgStars.toString() });
+      // const objToRate = JSON.parse(`{ rating${numreroLibro}: ${avgStars.toString} }`);
+      // console.log(objToRate + " Objeto");
+      // console.log(numreroLibro + " numreroLibro");
+      ///this.starForm.setValue(objToRate);
     }
+    const libroValor = {
+      libro: libro,
+      valoracion: avgStars
+    }
+    console.log(Object.values(libroValor));
 
+      this.librosValoraciones.push(libroValor);
+      console.log(Object.values(this.librosValoraciones) + " Array");
   }
 
   setValorationBooks(libros: Book[]): void{
