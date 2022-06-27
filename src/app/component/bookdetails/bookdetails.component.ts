389bdf84f4abcaba9466b3dd50a3ba14601dd8ff
adriaqf <adriaqf@gmail.com>
2022-06-27 15:04:08 +0200

    bookdetails simulacion pegote de data

--- src/app/component/bookdetails/bookdetails.component.ts
+++ src/app/component/bookdetails/bookdetails.component.ts
@@ -13,32 +13,32 @@ export class BookdetailsComponent implements OnInit {
   starForm = new FormGroup({
     rating: new FormControl(),
   });
-  libroSimulado ={
-    "id": 11,
-    "autor": "Arturo Perez",
-    "titulo": "El camino del fuego",
-    "isbn": "0-7645-2641-2",
-    "edad": 18,
-    "categoria": "Ficcion",
-    "cantidad_veces_reservado": 0,
-    "url_img": "../../../assets/img/harry.jpg",
-    "descripcion": "",
-    "disponible": "1",
-    "usuario": {
-        "id": 11,
-        "username": "marc",
-        "email": "marc@gmail.com",
-        "password": "$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.",
-        "role": "ADMIN",
-        "edad": "2022-01-12",
-        "url_imagen": "",
-        "activo": "1",
+  libroSimulado = {
+    id: 11,
+    autor: 'Arturo Perez',
+    titulo: 'El camino del fuego',
+    isbn: '0-7645-2641-2',
+    edad: 18,
+    categoria: 'Ficcion',
+    cantidad_veces_reservado: 0,
+    url_img: 'https://covers.openlibrary.org/b/id/240727-L.jpg',
+    descripcion: 'paseo por el mar',
+    disponible: '1',
+    usuario: {
+      id: 11,
+      username: 'marc',
+      email: 'marc@gmail.com',
+      password: '$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.',
+      role: 'ADMIN',
+      edad: '2022-01-12',
+      url_imagen: '',
+      activo: '1',
     },
-    "editorial": {
-        "id": 31,
-        "nombre": "Nordicos"
-    }
-}
+    editorial: {
+      id: 31,
+      nombre: 'Nordicos',
+    },
+  };
 
   botoSubmit() {
     console.log('valor radio ' + this.starForm.get('rating')?.value);
@@ -59,9 +59,8 @@ export class BookdetailsComponent implements OnInit {
     this.serviceBook.getById(id).subscribe({
       next: (result: Book) => {
         //this.libro = result;
-       this.libro = this.libroSimulado;
-       console.log('valor libro :',this.libro);
-
+        this.libro = this.libroSimulado;
+        console.log('valor libro :', this.libro);
       },
       error: (error: any) => {
         console.log(error);
@@ -70,4 +69,15 @@ export class BookdetailsComponent implements OnInit {
   }
 
   getAllValoracion() {}
+
+  getValueLibroDisponible(dispponible: string): string {
+    switch (dispponible) {
+      case '0':
+        return 'No disponible';
+      case '1':
+        return 'Disponible';
+      default:
+        return '';
+    }
+  }
 }
